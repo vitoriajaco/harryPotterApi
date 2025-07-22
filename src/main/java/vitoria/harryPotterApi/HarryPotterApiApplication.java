70d1a6d792ada967204763a3031d54b0fc4f873b
vitoriajaco <vitoriajaco@gmail.com>
2025-07-22 19:04:02 -0300

    first commit

---
+++ src/main/java/vitoria/harryPotterApi/HarryPotterApiApplication.java
@@ -0,0 +1,13 @@
+package vitoria.harryPotterApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarryPotterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarryPotterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vitoria/harryPotterApi/HarryPotterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vitoria.harryPotterApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarryPotterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
